78c2b593463b3abcaa035423d926a1d572303b8a
Axel Koolhaas <axel.koolhaas@hotmail.com>
2023-08-16 15:10:19 +0200

    added code

--- Sources/VLClap/Tweak.x.swift
+++ Sources/VLClap/Tweak.x.swift
@@ -1,2 +1,10 @@
 import Orion
 import VLClapC
+
+class LabelHook: ClassHook<UILabel> {
+    func setText(_ text: String) {
+        orig.setText(
+            text.uppercased().replacingOccurrences(of: " ", with: "👏")
+        )
+    }
+}
